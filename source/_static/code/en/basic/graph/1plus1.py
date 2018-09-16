5fd350098bc6a75db0f913904761db66c2543116
dpinthinker <dpinthinker@gmail.com>
2018-09-16 11:57:41 +0800

    Update translation

--- source/_static/code/en/basic/graph/1plus1.py
+++ source/_static/code/en/basic/graph/1plus1.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # Defince a "Computation Graph"
 a = tf.constant(1)  # Defince a constant Tensor
 b = tf.constant(1)
-c = a + b  # equal to c = tf.add(a, b)，c is a new Tensor created by Tensor a and Tesor b's add Operation
+c = a + b  # Equal to c = tf.add(a, b)，c is a new Tensor created by Tensor a and Tesor b's add Operation
 
 sess = tf.Session()     # Initailize a Session
 c_ = sess.run(c)        # Session的run() will do actually computation to the nodes (Tensor) in the Computation Graph
